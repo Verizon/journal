81c39d67c40dfc99921094ca229013c001affe9d
tmperret <timothy.m.perrett@oncue.com>
2015-07-17 01:35:38 -0700

    hopefully this works!

--- project/custom.scala
+++ project/custom.scala
@@ -58,6 +58,7 @@ object custom {
       </developers>),
     publishMavenStyle := true,
     releaseCrossBuild := true,
+    useGpg := true,
     licenses := Seq("Apache-2.0" -> url("https://www.apache.org/licenses/LICENSE-2.0.html")),
     homepage := Some(url("http://oncue.github.io/journal")),
     scmInfo := Some(ScmInfo(url("https://github.com/oncue/journal"),
